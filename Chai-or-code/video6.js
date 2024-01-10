5ac30ce528e5748169fe69c24df19b6ebbe03c36
Md Tauhidul Islam Tonmoy <149892884+tauhidtgk@users.noreply.github.com>
2024-01-10 18:19:26 +0000

    Done

---
+++ Chai-or-code/video6.js
@@ -0,0 +1,13 @@
+// let score = 33;
+// console.log(score);
+// console.log(typeof score); - number show kortese
+// -----------------------------------------------------------------
+// let score = '33abc';
+ 
+// let scorev = Number(score);
+// console.log(scorev); -- NaN Ashtese aita js r akta problem tai amader sabdhan thakte be number ar conversion niye.
+// console.log(typeof scorev); // Number ashe.
+// ------------------------------------------------------------------
+// let score = null;
+// let scorev = Number(score);
+// console.log(scorev) -- console a 0 ashe;;
