11e3c95a5061ed732917ec0065d72dc454a6994e
Md Tauhidul Islam Tonmoy <149892884+tauhidtgk@users.noreply.github.com>
2024-01-10 14:10:02 +0000

    devcontainer

---
+++ Chai-or-code/variable.js
@@ -0,0 +1,10 @@
+const ok = 'okk';
+let ok2 = 'oooo';
+var ok3 = 'hmmm';
+hmm = "oree";
+
+ok2 = 'are';
+ok3 = 'ure';
+hmm = 'oho';
+
+console.table([ok, ok2, ok3, hmm]); // emne node e kaj kore kintu console e kaj kore na.
---
+++ Chai-or-code/video5.js
@@ -0,0 +1,3 @@
+//"use strict"; // it treats js code to newer versions.
+
+// alert(3+3); -- emne node e kaj kore na -- console e kaj kore.
